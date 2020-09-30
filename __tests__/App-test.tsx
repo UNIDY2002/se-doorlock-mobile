c82477b647b4c8055266bd847959c1e432ee6e76
UNIDY2002 <UNIDY2002@outlook.com>
2020-09-30 18:04:44 +0800

    Add tests for routing.

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -2,9 +2,34 @@ import "react-native";
 import React from "react";
 import {App} from "../src/App";
 import renderer, {act} from "react-test-renderer";
+import {Button} from "react-native";
 
-it("renders correctly", async () => {
-    await act(async () => {
-        renderer.create(<App />);
-    });
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+test("routing", async () => {
+    // Render the app
+    const {root} = renderer.create(<App />);
+    await act(async () => {});
+
+    // Press register
+    root.findAllByType(Button)[1].props.onPress();
+    await sleep(800);
+    expect(root.findAllByType(Button)[2].props.title).toEqual("返回登录");
+
+    // Press return to login
+    root.findAllByType(Button)[2].props.onPress();
+    await sleep(800);
+    expect(root.findAllByType(Button).length).toEqual(2);
+    expect(root.findAllByType(Button)[0].props.title).toEqual("登录");
+    expect(root.findAllByType(Button)[1].props.title).toEqual("注册");
+
+    // Press login
+    root.findAllByType(Button)[0].props.onPress();
+    await sleep(800);
+
+    // Switch between tabs
+    const UsersTab = root.findByProps({name: "Users"});
+    UsersTab.props.navigation.navigate("Devices");
+    UsersTab.props.navigation.navigate("Advanced");
+    await sleep(800);
 });

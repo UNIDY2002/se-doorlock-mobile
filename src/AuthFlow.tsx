79c84108552fac893eaf45f2395f2d15e34182e4
UNIDY2002 <UNIDY2002@outlook.com>
2020-09-30 14:55:04 +0800

    Add icons to bottom tab navigator

--- src/AuthFlow.tsx
+++ src/AuthFlow.tsx
@@ -11,6 +11,7 @@ import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import {UsersStack} from "./ui/users/usersStack";
 import {DevicesStack} from "./ui/devices/devicesStack";
 import {AdvancedStack} from "./ui/advanced/advancedStack";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 export type LoginStackParamList = {
     Login: undefined;
@@ -25,7 +26,33 @@ export type LoginNav = StackNavigationProp<LoginStackParamList>;
 
 export const AuthFlowComponent = ({loggedIn}: {loggedIn: boolean}) =>
     loggedIn ? (
-        <Tab.Navigator>
+        <Tab.Navigator
+            screenOptions={({route}) => ({
+                tabBarIcon: ({color, size}) => {
+                    let iconName;
+
+                    switch (route.name) {
+                        case "Users":
+                            iconName = "user";
+                            break;
+                        case "Devices":
+                            iconName = "unlock-alt";
+                            break;
+                        case "Advanced":
+                            iconName = "compass";
+                            break;
+                    }
+
+                    return (
+                        <Icon name={iconName || ""} size={size} color={color} />
+                    );
+                },
+            })}
+            backBehavior="initialRoute"
+            tabBarOptions={{
+                activeTintColor: "tomato",
+                inactiveTintColor: "gray",
+            }}>
             <Tab.Screen name="Users" component={UsersStack} />
             <Tab.Screen name="Devices" component={DevicesStack} />
             <Tab.Screen name="Advanced" component={AdvancedStack} />

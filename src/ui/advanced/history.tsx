d5030d17e14d03a05dca68bd568b343e83d80eca
UNIDY2002 <UNIDY2002@outlook.com>
2020-11-02 15:40:58 +0800

    Integrate ui with redux and setup test (#12).

--- src/ui/advanced/history.tsx
+++ src/ui/advanced/history.tsx
@@ -9,17 +9,26 @@ import Snackbar from "react-native-snackbar";
 import Icon from "react-native-vector-icons/FontAwesome";
 import Swipeable from "react-native-gesture-handler/Swipeable";
 import {AdvancedNav} from "./advancedStack";
+import {Activity} from "../../redux/states/config";
+import {REMOVE_ACTIVITY} from "../../redux/constants";
+
+const pad = (data: number) => {
+    const str = String(data);
+    return str.length === 2 ? str : `0${str}`;
+};
 
 const ActivitiesUI = ({
     activities,
     navigation,
+    remove,
 }: {
-    activities: any[];
+    activities: Activity[];
     navigation: AdvancedNav;
+    remove: (activity: Activity) => void;
 }) => (
     <FlatList
         data={activities}
-        renderItem={(_) => (
+        renderItem={({item}) => (
             <Swipeable
                 renderRightActions={() => (
                     <RectButton
@@ -33,8 +42,9 @@ const ActivitiesUI = ({
                                 "您确定要删除吗？",
                                 "该操作无法撤销",
                                 () => {
+                                    remove(item);
                                     Snackbar.show({
-                                        text: "处理中……",
+                                        text: "已删除",
                                         duration: Snackbar.LENGTH_SHORT,
                                     });
                                 },
@@ -47,13 +57,28 @@ const ActivitiesUI = ({
                     style={Material.card}
                     onPress={() => navigation.navigate("AdvancedHistory", {})}
                     testID="activityItem">
-                    <Text style={{fontSize: 20, fontWeight: "bold"}}>打卡</Text>
+                    <Text style={{fontSize: 20, fontWeight: "bold"}}>
+                        重复：{item.repeat}
+                    </Text>
+                    <Text style={{fontSize: 16}}>
+                        起始时间：{pad(item.beginHour)}:{pad(item.beginMinute)}
+                    </Text>
+                    <Text style={{fontSize: 16}}>
+                        终止时间：{pad(item.endHour)}:{pad(item.endMinute)}
+                    </Text>
                 </TouchableOpacity>
             </Swipeable>
         )}
+        keyExtractor={({repeat, beginHour, beginMinute, endHour, endMinute}) =>
+            `[${repeat}]${beginHour}:${beginMinute}-${endHour}:${endMinute}`
+        }
     />
 );
 
-export const ActivitiesScreen = connect((_: State) => ({activities: []}))(
-    ActivitiesUI,
-);
+export const ActivitiesScreen = connect(
+    (state: State) => state.config,
+    (dispatch) => ({
+        remove: (activity: Activity) =>
+            dispatch({type: REMOVE_ACTIVITY, payload: activity}),
+    }),
+)(ActivitiesUI);

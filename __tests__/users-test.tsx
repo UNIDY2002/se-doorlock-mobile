0d9acb05cd2aa2733539bb98626de5e22dd225fe
UNIDY2002 <UNIDY2002@outlook.com>
2020-11-11 17:36:39 +0800

    Adjust testing code (#14).

--- __tests__/users-test.tsx
+++ __tests__/users-test.tsx
@@ -1,26 +1,9 @@
-import "react-native";
-import React from "react";
-import {App} from "../src/App";
-import renderer from "react-test-renderer";
-import {Button} from "react-native";
-import {TextInput} from "react-native";
+import {renderAndLogin, sleep} from "../src/utils/testUtils";
 import {RectButton} from "react-native-gesture-handler";
-
-const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 test("users", async () => {
     jest.setTimeout(30000);
-
-    // Render the app
-    const {root} = renderer.create(<App />);
-    await sleep(1000);
-
-    // Perform login
-    root.findAllByType(TextInput)[0].props.onChangeText("super");
-    root.findAllByType(TextInput)[1].props.onChangeText("123456");
-    await sleep(800);
-    root.findAllByType(Button)[0].props.onPress();
-    await sleep(1500);
+    const root = await renderAndLogin();
 
     // Perform add user
     root.findByProps({testID: "userAddTopRight"}).props.onPress();
